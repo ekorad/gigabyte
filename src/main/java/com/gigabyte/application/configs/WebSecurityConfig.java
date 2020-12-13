fe5b15e0b2344b7d93589c0351d3b38990c2ed9b
Vlad Zahiu <vladzahiu28@gmail.com>
2020-12-13 19:22:26 +0200

    Changed web security configuration
    Security configuration now accomodates a password authentication filter

--- src/main/java/com/gigabyte/application/configs/WebSecurityConfig.java
+++ src/main/java/com/gigabyte/application/configs/WebSecurityConfig.java
@@ -1,32 +1,62 @@
 package com.gigabyte.application.configs;
 
 import com.gigabyte.application.others.Http401UnauthorizedEntryPoint;
+import com.gigabyte.application.services.WebUserDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity(debug = true)
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     @Autowired
     private Http401UnauthorizedEntryPoint authEntryPoint;
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new WebUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+
+    @Bean
+    public UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter() throws Exception {
+        UsernamePasswordAuthenticationFilter filter = new UsernamePasswordAuthenticationFilter(authenticationManager());
+        filter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/users/authenticate", "POST"));
+
+        return filter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .httpBasic().disable().formLogin().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeRequests().antMatchers("/users/authenticate").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .exceptionHandling().authenticationEntryPoint(authEntryPoint);
+        http.csrf().disable().httpBasic().disable().formLogin().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+                .antMatchers("/users/authenticate").permitAll().anyRequest().authenticated().and().exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint);
     }
 
 }
